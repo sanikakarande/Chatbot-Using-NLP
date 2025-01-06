fead94b174feb489e131d71083bc76f619edc7d4
sanikakarande <129505375+sanikakarande@users.noreply.github.com>
2025-01-06 18:55:35 +0530

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -15,12 +15,14 @@ class Message:
     message: str
 
 def load_css():
+    css_path = os.path.join(os.path.dirname(__file__), "static", "style.css")
     try:
-         with open("static/style.css", "r") as f:
+        with open(css_path, "r") as f:
             css = f"<style>{f.read()}</style>"
             st.markdown(css, unsafe_allow_html=True)
-    except FileNotFoundError: st.error("CSS file not found.") 
-    except Exception as e: st.error(f"An error occurred: {e}")
+    except FileNotFoundError:
+        st.error(f"CSS file not found at {css_path}.")
+
 
 def initialize_session_state():
     if "history" not in st.session_state:
